57ddc8e4698f17027aa65ef93f0042b8fcb3181c
Pablo Martin <caedes@sindominio.net>
2013-12-21 03:13:15 +0100

    Add per-address balance keeping and link notifications to external web service.

---
+++ cypher.py
@@ -0,0 +1,55 @@
+"""
+Checking gpg signatures
+"""
+
+import time
+from pyme import core, constants, errors, pygpgme
+from pyme.constants.sig import mode
+
+def sign_text(text, key_uid):
+    ciphertext = core.Data()
+    plaintext = core.Data(text)
+  
+    ctx = core.Context()
+  
+    ctx.set_armor(1)
+    #ctx.set_passphrase_cb(_passphrase_callback)
+  
+    ctx.op_keylist_start(key_uid, 0)
+    sigkey = ctx.op_keylist_next()
+    # print sigkey.uids[0].uid
+  
+    ctx.signers_clear()
+    ctx.signers_add(sigkey)
+  
+    ctx.op_sign(plaintext, ciphertext, mode.NORMAL)
+  
+    ciphertext.seek(0, 0)
+    signature = ciphertext.read()
+  
+    return signature
+
+def check_signature(text, fingerprint):
+    gpg = core.Context()
+    plaintext = core.Data()
+    signed = core.Data(text)
+    try:
+        res = gpg.op_verify(signed, None, plaintext)
+    except errors.GPGMEError:
+        # incorrect data
+        return False
+        
+    plaintext.seek(0,0)
+    result_data = plaintext.read()
+
+    res = gpg.op_verify_result()
+
+    s = res.signatures[0]
+
+    if s.fpr == fingerprint:
+        return result_data, s.timestamp
+    else:
+        print 'invalid fingerprint', s.fpr
+        return False
+
+
--- main.py
+++ main.py
@@ -10,6 +10,43 @@ import obelisk
 from twisted.internet import reactor
 from obelisk.util import to_btc
 
+# post url
+url = 'http://localhost:8080/privateapi/update_balance'
+
+# key to sign with
+key_id = 'FFFFFFFF'
+
+# Address History
+class AddressHistory(object):
+    def __init__(self, address):
+        self.address = address
+        self.balance = 0
+        self.unconfirmed = 0
+        self.history = {}
+
+    def add_output(self, tx_hash, height, value):
+        self.history[tx_hash] = [height, value]
+        self.balance = self.get_balance()
+        self.unconfirmed = self.get_unconfirmed()
+
+    def get_balance(self):
+        total = 0
+        for height, value in self.history.values():
+            if height:
+                total += value
+        return total
+
+    def get_unconfirmed(self):
+        total = 0
+        for height, value in self.history.values():
+            if not height:
+                total += value
+        return total
+
+    def __str__(self):
+        return "AddressHistory('%s', %s, %s)" % (self.address, self.balance, self.unconfirmed)
+
+
 ##############################################
 # Service class
 
@@ -26,14 +63,13 @@ class Monitor(obelisk.ObeliskOfLightClient):
 
     def parse_history(self, ec, history, address):
         """Receive history for an address from the server"""
-        total = 0
+        self._addresses[address] = AddressHistory(address)
         for row in history:
             o_hash, o_index, o_height, value, s_hash, s_index, s_height = row
             if s_height == 0xffffffff:
-                total += value
-        if total > 0:
-            print address, to_btc(total)
-        self._addresses[address] = total
+                self._addresses[address].add_output(o_hash.encode('hex'), o_height, value)
+        if self._addresses[address].balance > 0:
+            print address, to_btc(self._addresses[address].balance), self._addresses[address]
 
     ##############################################
     # Loading addresses
@@ -45,8 +81,8 @@ class Monitor(obelisk.ObeliskOfLightClient):
             self.parse_history(_ec, _history, address)
             if self.naddresses == 0:
                 self.subscribe_addresses(self.file_name)
-            if self.naddresses % 1000 == 0:
-                print "loaded", 10000-self.naddresses
+            if True: #self.naddresses % 1000 == 0:
+                print "loaded", 6-self.naddresses, address
             
         self.naddresses += 1
         self.fetch_history(address, _on_history)
@@ -56,8 +92,8 @@ class Monitor(obelisk.ObeliskOfLightClient):
         f = open(file_name, 'r')
         for i, address in enumerate(f.readlines()):
             self.load_address(address.strip())
-            if i%1000 == 0:
-                print 'loading',i
+            if True: #i%1000 == 0:
+                print 'loading', i, address
         f.close()
 
     def subscribe_addresses(self, file_name):
@@ -65,8 +101,8 @@ class Monitor(obelisk.ObeliskOfLightClient):
         f = open(file_name, 'r')
         for i, address in enumerate(f.readlines()):
             self.subscribe_address(address.strip(), self.on_address_update)
-            if i%1000 == 0:
-                print 'subscribed',i
+            if True: # i%1000 == 0:
+                print 'subscribed', i, address
         f.close()
 
 
@@ -76,13 +112,17 @@ class Monitor(obelisk.ObeliskOfLightClient):
     def on_address_update(self, address_version, address_hash, height, block_hash, raw_tx):
         """Callback for address update, receives transactions relevant to the address."""
         address = obelisk.bitcoin.hash_160_to_bc_address(address_hash, address_version), height
-        print "update for address", address[0]
         tx = obelisk.bitcoin.Transaction(raw_tx.encode('hex'))
-        print "  inputs", len(tx.inputs), tx.inputs[0]['address']
         for output in tx.outputs:
-            print " - output", output
             if address[0] == output[0]:
-                print "  output", output[1]
+                print "update for address", address[0], output[1], height
+                if not address[0] in self._addresses:
+                    self._addresses[address[0]] = AddressHistory(address[0])
+                tx_hash = tx.hash()
+                history = self._addresses[address[0]]
+                history.add_output(tx_hash, height, output[1])
+                print tx_hash, history
+                self.balance_changed(address[0], [history.get_balance(), history.get_unconfirmed()])
 
 
     ##############################################
@@ -90,10 +130,13 @@ class Monitor(obelisk.ObeliskOfLightClient):
 
     def balance_changed(self, address, balances):
         """Notify external service about changes to an address"""
+        import cypher
+        import urllib
+        import urllib2
         if balances == [0, 0]:
            return
         timestamp = time.time()
-        print address, balances, timestamp
+        print "balance changed", address, balances, timestamp
         args = {'timestamp': timestamp,
                 'balance': balances[0],
                 'balance2': balances[1],
@@ -128,7 +171,7 @@ class Monitor(obelisk.ObeliskOfLightClient):
         print "* tx", hash.encode('hex'), ", ".join(outputs), dir(tx)
  
 if __name__ == '__main__':
-    c = Monitor('tcp://85.25.198.97:9091', 'tcp://85.25.198.97:9093')
+    c = Monitor('tcp://85.25.198.97:8081', 'tcp://85.25.198.97:8083')
     # some popular addresses for testing subscription
     c.subscribe_address("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp", c.on_address_update)
     c.subscribe_address("1dice97ECuByXAvqXpaYzSaQuPVvrtmz6", c.on_address_update)
